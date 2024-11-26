c1bfeda8b0a2987ab81f7d2e543750746c1ee77d
tgvie <vy.petersson@gmail.com>
2024-11-26 05:36:06 +0100

    Change import path

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { products } from "/js/products.js";
+import { products } from "./js/products.js";
 
 // ---------- TARGET HTML PRODUCTS PAGE ----------
 const productsPage = document.querySelector("#products-page");
