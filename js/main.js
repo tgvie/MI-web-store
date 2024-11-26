dae1b6bbc59f02a2f1b5a1c518525ff957f091b5
tgvie <vy.petersson@gmail.com>
2024-11-26 05:14:56 +0100

    Add functions to handle adding products to cart and update order summary

---
+++ js/main.js
@@ -0,0 +1,109 @@
+import { products } from "/js/products.js";
+
+// ---------- TARGET HTML PRODUCTS PAGE ----------
+const productsPage = document.querySelector("#products-page");
+
+/*
+ * PRINT PRODUCTS
+ ** for each object in the array
+ ** create corresponding HTML-elements
+*/
+products.forEach((item) => {
+  productsPage.innerHTML += `
+    <article class="item-card">
+        <img src="${item.img.url}" alt="${item.img.alt}">
+        <h2>${item.name}</h2>
+        <h3>${item.category}</h3>
+        <p>${item.price} kr</p>
+        <p>${item.rating}</p>
+
+        <!-- PLUS-MINUS BUTTONS -->
+        <div class="plus-minus-btn" id="product-${item.id}">
+            <button class="minus-btn" data-id="${item.id}" aria-label="Decrease quantity">-</button>
+            <span id="total-${item.id}">0</span>
+            <button class="plus-btn" data-id="${item.id}" aria-label="Increase quantity">+</button>
+        </div>
+
+        <!-- ADD TO CART BUTTON -->
+        <button class="add-cart-btn" data-id="${item.id}">Add to Cart</button>
+    </article>
+    `;
+});
+
+// ---------- CLICK-EVENTS WITHIN PRODUCTS CONTAINER ----------
+productsPage.addEventListener("click", (e) => {
+  // When plus-btn is clicked
+  if (e.target.classList.contains("plus-btn")) {
+    const productId = e.target.getAttribute("data-id"); //Target product-id
+    let totalSum = document.getElementById(`total-${productId}`); //Target html-<span> that displays total count
+    let currentTotal = parseInt(totalSum.innerText); //Get the total and convert to number
+    totalSum.innerText = currentTotal + 1; //Increase total by 1 and update it visually
+  }
+
+  // When minus-btn is clicked
+  if (e.target.classList.contains("minus-btn")) {
+    const productId = e.target.getAttribute("data-id"); //Target product-id
+    let totalSum = document.getElementById(`total-${productId}`); //Target html-<span> that displays total count
+    let currentTotal = parseInt(totalSum.innerText); //Get the total and convert to number
+    if (currentTotal > 0) {
+      //Decrease total by 1 only if current total is less than 0, so there is no -number
+      totalSum.innerText = currentTotal - 1;
+    }
+  }
+
+  // When Add to Cart button is clicked
+  if (e.target.classList.contains("add-cart-btn")) {
+    const productId = parseInt(e.target.getAttribute("data-id")); //Get id for which product was clicked and convert it to number
+    const productAmount = document.getElementById(`total-${productId}`); //Use the product-id that shows the total of that product
+    const currentTotal = parseInt(productAmount.innerText); //Get the current total from HTML element and convert to number, how many user want to add
+
+    if (currentTotal > 0) { //Only triggers if there are more than 0 item
+      addToCart(productId, currentTotal);
+
+      productAmount.innerText = 0; //Reset the amount visually so user knows product has been added
+    }
+  }
+});
+
+let cart = [];
+
+// ---------- TO ADD PRODUCT TO CART ----------
+function addToCart(productId, totalAmount) {
+  // Search cart-array to see if the product is already in cart
+  const productInCart = cart.findIndex((item) => item.id === productId); //Callback function to specify the condition to find the product to add
+
+  if (productInCart > -1) { //If product is already in cart (if the returned index from Ln 73 is 0 or higher)
+    cart[productInCart].amount += totalAmount; //Update its amount
+  }
+
+  // Else if product not in cart, add it
+  else {
+    const addProduct = products.find((item) => item.id === productId); //Find in products-array the item(id) to add
+    if (addProduct) {
+      // If found, add product to cart
+      cart.push({ ...addProduct, amount: totalAmount }); //... operator to duplicate the object(product) to visiually add its info to the cart
+    }
+  }
+
+  updateOrderSummary(); 
+}
+
+// ---------- UPDATE ORDER SUMMARY VISUALLY ----------//
+function updateOrderSummary() {
+  const orderSummary = document.querySelector("#order-summary");
+
+  orderSummary.innerHTML = "<h2>Order Summary</h2>";
+
+   if (cart.length === 0) { //If no items in cart
+    orderSummary.innerHTML += "<p>Your cart is empty.</p>";
+    return;
+  } 
+
+  cart.forEach((item) => {
+    orderSummary.innerHTML += `
+        <div class="order-item">
+          <p>${item.amount} x ${item.name} - ${item.price * item.amount} kr</p>
+        </div>
+      `;
+  });
+}
--- js/products.js
+++ js/products.js
@@ -1,29 +1,30 @@
-const products = [
+export const products = [
   {
     id: 112, 
     name: "The Nine-Tailed Fox",
     price: 429,
     category: "Spirit",
     rating: 3.5,
+    amount: 0, 
     img: {
       url: "assets/images/product-images/product1.png",
       width: 1024,
       height: 1024,
       alt: "Chibi-style figurine of a nine-tailed fox character wearing a colorful kimono with floral patterns and a mix of pink, blue, and golden accents, creating a charming and vibrant look. The figure has long flowing soft pink hair, fox ears, and a friendly expression. The tails behind the figure resemble petals with an elegant and mystical feelings.",
     },
-  },
-  {
+  },  {
     id: 123, 
     name: "The Sinister Blade",
     price: 399,
     category: "Warrior",
     rating: 5,
+    amount: 0, 
     img: {
       url: "assets/images/product-images/product2.png",
       width: 1024,
       height: 1024,
       alt: "Chibi-style figurine of a fierce character with red fox ears and flowing crimson hair, with a floral hair accessory on the right ear that adds a touch of elegance to the overall look. The figure wears a kimono-inspired outfit with intricate blue and pink patterns, along with a vibrant red obi belt. The character has a determined expression with striking red eyes, exuding an aura of strength and confidence.",
-    },
+    }
   },
   {
     id: 134, 
@@ -31,12 +32,13 @@ const products = [
     price: 449,
     category: "Spirit",
     rating: 4.5,
+    amount: 0, 
     img: {
       url: "assets/images/product-images/product3.png",
       width: 1024,
       height: 1024,
       alt: "Chibi-style figurine of a mystical character with fox-like ears and a white hooded cloak. The character has long, flowing silver hair and piercing blue eyes, giving feelings of mystery and magic. The hood features ornate golden designs, and the outfit is in muted purples and blues. The character is accompanied by a shadowy fox-like companion with glowing eyes and complements the character, adding a touch of darkness and mystique.",
-    },
+    }
   },
   {
     id: 145, 
@@ -44,12 +46,13 @@ const products = [
     price: 379,
     category: "Warrior",
     rating: 3.5,
+    amount: 0, 
     img: {
       url: "assets/images/product-images/product4.png",
       width: 1024,
       height: 1024,
       alt: "Chibi-style figurine of a fierce huntress with fox-like ears, holding a large spear adorned with tassels. The character has long brown hair, a determined expression, and wears earth-toned clothing with tribal patterns. The outfit includes a fur-trimmed cape, detailed belt, and traditional accessories, giving a rugged and warrior-like appearance. The character exudes confidence and strength, capturing the spirit of a skilled protector of the wilderness.",
-    },
+    }
   },
   {
     id: 156, 
@@ -57,25 +60,26 @@ const products = [
     price: 389,
     category: "Warrior",
     rating: 4,
+    amount: 0, 
     img: {
       url: "assets/images/product-images/product5.png",
       width: 1024,
       height: 1024,
       alt: "Chibi-style figurine of a whimsical character with long, flowing silver hair, wearing a vibrant turquoise with gold accents and a red sash tied around the waist. The figure features colorful feathers in the hair, creating a playful and lively look. The poses a calm expression with bright peaceful eyes, giving feelings of freedom and adventure.",
     },
-  },
-  {
+  },  {
     id: 167, 
     name: "The Fallen",
     price: 449,
     category: "Celestial",
     rating: 5,
+    amount: 0, 
     img: {
       url: "assets/images/product-images/product6.png",
       width: 1024,
       height: 1024,
       alt: "Chibi-style figurine of a mysterious character with dark wings and flowing black hair. The figure has striking red eyes, and wears a kimono-inspired outfit in shades of deep purple and blue with intricate patterns. The character is equipped with floral accessories and tassels. The character poses serious expression, giving a sense of power and dark allure, along with the wings.",
-    },
+    }
   },
   {
     id: 178, 
@@ -83,12 +87,13 @@ const products = [
     price: 339,
     category: "Spirit",
     rating: 3,
+    amount: 0, 
     img: {
       url: "assets/images/product-images/product7.png",
       width: 1024,
       height: 1024,
       alt: "Chibi-style figurine of a cheerful fish-like character inspired by aquatic elements. The character has blue scales, large bright eyes, and vibrant orange fins. The outfit is a kimono with flowing sleeves featuring intricate wave patterns in blue, pink, and gold. The characters happy expression and colorful design evoke a playful underwater theme, giving the figure a whimsical and charming look.",
-    },
+    }
   },
   {
     id: 189, 
@@ -96,12 +101,13 @@ const products = [
     price: 529,
     category: "Celestial",
     rating: 5,
+    amount: 0, 
     img: {
       url: "assets/images/product-images/product8.png",
       width: 1024,
       height: 1024,
       alt: "Chibi-style figurine of an ethereal character with long, flowing blonde hair, wearing a flowing robe in pastel blue, pink, and white hues, with delicate floral and star-themed accessories that features intricate celestial patterns. The character has bright blue eyes and a serene expression, evoking a sense of magic and connection to the stars. The background includes soft celestial motifs, enhancing the graceful appearance.",
-    },
+    }
   },
   {
     id: 190, 
@@ -109,12 +115,13 @@ const products = [
     price: 319,
     category: "Celestial",
     rating: 2,
+    amount: 0, 
     img: {
       url: "assets/images/product-images/product9.png",
       width: 1024,
       height: 1024,
       alt: "Chibi-style figurine of a cheerful young boy riding on the shoulders of a large, friendly yeti. The boy wears a cute hat with animal ears and a colorful winter outfit, while the yeti is adorned in blue attire with intricate patterns and a joyful expression. The characters pose a sense of adventure and fun in a snowy landscape, with the boy and the yeti sharing a moment of happiness together.",
-    },
+    }
   },
   {
     id: 191, 
@@ -122,60 +129,12 @@ const products = [
     price: 429,
     category: "Warrior",
     rating: 4,
+    amount: 0, 
     img: {
       url: "assets/images/product-images/product10.png",
       width: 1024,
       height: 1024,
       alt: "Chibi-style figurine of a mysterious warrior character with flowing silver and black hair, holding a katana. The figure has an intense expression with piercing blue eyes and wears a dark robe with purple and teal accents with intricate designs. The character is adorned with black wings, giving an aura of power and mystique. The pose conveys a sense of grace, strength, and an enigmatic presence.",
-    },
-  },
+    }
+  }
 ];
-
-// ---------- TARGET HTML DIV CONTAINER ----------
-const productsPage = document.querySelector("#products-page");
-
-/*
-* PRINT PRODUCTS 
-** for each object in the array
-** create corresponding HTML-elements
-*/
-products.forEach((item) => {
-  productsPage.innerHTML += `
-    <article class="item-card">
-        <img src="${item.img.url}" alt="${item.img.alt}">
-        <h2>${item.name}</h2>
-        <h3>${item.category}</h3>
-        <p>${item.price} kr</p>
-        <p>${item.rating}</p>
-
-        <!-- PLUS-MINUS BUTTONS -->
-        <div class="plus-minus-btn" id="product-${item.id}">
-            <button class="minus-btn" data-id="${item.id}" aria-label="Decrease quantity">-</button>
-            <span id="total-${item.id}">0</span>
-            <button class="plus-btn" data-id="${item.id}" aria-label="Increase quantity">+</button>
-        </div>
-    </article>
-    `;
-});
-
-// ---------- BUTTON CLICKS TO DECREASE-INCREASE ----------
-productsPage.addEventListener('click', (e) => {
-
-    // Check if the clicked element is plus-btn
-    if (e.target.classList.contains("plus-btn")) {
-        const productId = e.target.getAttribute("data-id"); //Target product-id
-        let totalSum = document.getElementById(`total-${productId}`); //Target html-<span> that displays the total
-        let currentTotal = parseInt(totalSum.innerText); //Get the total and convert to number
-        totalSum.innerText = currentTotal + 1; //Increase total by 1 and update it visually
-    }
-
-    // Check if the clicked element is minus-btn
-    if (e.target.classList.contains("minus-btn")) {
-        const productId = e.target.getAttribute("data-id"); //Target product-id
-        let totalSum = document.getElementById(`total-${productId}`); //Target html-<span> that displays the total
-        let currentTotal = parseInt(totalSum.innerText); //Get the total and convert to number
-        if (currentTotal > 0) { //Decrease total by 1 only if current total is less than 0, so there is no -number
-            totalSum.innerText = currentTotal -1;
-        }
-    }
-});
